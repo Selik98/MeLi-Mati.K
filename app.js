413e6c9d044d1604838567f6c66f9bddec27fc15
Selik98 <mkiesel98@gmail.com>
2023-06-27 22:58:08 -0300

    Solo HTML

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const path = require ('path')
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    console.log ('Hicieron un Request en "/"')
+    res.sendFile(path.join(__dirname, './index.html'))
+});
+
+
+
+
+app.listen(5000, () => {
+    console.log ("Servidor escuchando Puerto 5000")
+}) 
+
+
+app.use(express.static('public'));
