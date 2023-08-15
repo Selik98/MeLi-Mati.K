ad61628d9bc055cda09ed651c04610d992b94a7a
Selik98 <mkiesel98@gmail.com>
2023-08-15 12:51:04 -0300

    Base de datos reducida

--- source/routes/productRouter.js
+++ source/routes/productRouter.js
@@ -1,16 +1,16 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/productController');
-const multer = require ('multer')
+const multer = require('multer')
 const path = require('path')
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb (null, path.join(__dirname, "../../public/img/products"))
+        cb(null, path.join(__dirname, "../../public/img/products"))
     },
-    filename: (req,file,cb) =>{
+    filename: (req, file, cb) => {
         console.log(file)
         const newfilename = Date.now() + '-' + file.originalname
-        console.log (newfilename)
+        console.log(newfilename)
         cb(null, newfilename)
     }
 })
@@ -21,7 +21,7 @@ const upload = multer({ storage });
 router.get('/products', productController.products);
 router.get('/:id/detail', productController.getDetail);
 router.get('/create', productController.crear);
-router.post('/crearProducto', upload.single('img') ,productController.createProduct);
+router.post('/crearProducto', upload.single('img'), productController.createProduct);
 
 
 module.exports = router;
